636c596ab0d271e07e1c48bf48822229a244b425
Akash Singh <akash@pop-os.localdomain>
2023-04-27 23:10:07 +0530

    payment added

--- payments/urls.py
+++ payments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import StripeCheckoutView
+from .views import StripeCheckoutView,get_status
 
 
 urlpatterns = [
     path('create-checkout-session',StripeCheckoutView.as_view()),
+    path('check-status/<str:session_id>',get_status),
 ]
--- payments/views.py
+++ payments/views.py
@@ -8,7 +8,9 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import redirect
-
+from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from mess.models import *
 
 class StripeCheckoutView(APIView):
     def post(self,request):
@@ -36,11 +38,34 @@ class StripeCheckoutView(APIView):
                 'error':'Something went wrong when creating stripe checkout session'},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
-    def get(self, request):
-        session_id = request.GET.get('session_id')
+
+@api_view(['POST'])
+def get_status(request,session_id):
+        # session_id = request.GET.get('session_id')
+        print(session_id)
         try:
             session = stripe.checkout.Session.retrieve(session_id)
+            print(session)
             payment_status = session.payment_status
+            if payment_status == 'paid':
+                username=request.data['username']
+                user=User.objects.get(username=username)
+                token=request.data['token']
+                if token=='0':
+                    date=request.data['date']
+                    time=request.data['time']
+
+                    SilverToken.objects.create(user=user,tokenDate=date,tokenTime=time)
+                else:
+                    goldToken=GoldToken.objects.get(user=user)
+                    if token=='1':
+                        goldToken.TokenCount+=45
+                        goldToken.save()
+                    else:
+                        goldToken.TokenCount+=90
+                        goldToken.save()
+                     
+                 
             return Response({'status': payment_status})
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
